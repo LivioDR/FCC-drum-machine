7f0ba935b0478f4b3155fae12ce86e228bef5ee1
LivioDR <liviodavidreinoso@yahoo.com.ar>
2024-04-23 16:29:27 -0400

    added tests

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <main>
       <DrumMachine/>
-      <script href="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+      {/* Disabled due to incompatibility with React18+ */}
+      {/* <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script> */}
     </main>
   );
 }
